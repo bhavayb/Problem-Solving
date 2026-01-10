35975821cc1b2fb57c9258a1eed49439cbe6f5e5
Bhavay Batra <btech10549.23@bitmesra.ac.in>
2026-01-10 16:54:49 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (5.50%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,17 +1,34 @@
 class Solution {
-    int func(vector<vector<int>>& matrix, int i, int j ,vector<vector<int>>& dp){
+    // int func(vector<vector<int>>& matrix, int i, int j ,vector<vector<int>>& dp){
         
-        if((i>0 && j< 0) || (i<0 && j>0))return 0;
+    //     // if((i>0 && j< 0) || (i<0 && j>0))return 0;
         
-        if(matrix[i][j] == 1) return 0;
-        if(i==0 && j ==0) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = func(matrix, i-1, j, dp) + func(matrix, i,j-1, dp);
-    }
+    //     // if(matrix[i][j] == 1) return 0;
+    //     // if(i==0 && j ==0) return 1;
+    //     // if(dp[i][j] != -1) return dp[i][j];
+    //     // return dp[i][j] = func(matrix, i-1, j, dp) + func(matrix, i,j-1, dp);
+    // }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return func(matrix, m-1, n-1, dp);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        // return func(matrix, m-1, n-1, dp);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j <n ; j++){
+                if(matrix[i][j] == 1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                else if(i == 0&& j == 0) dp[0][0] = 1;
+                else{
+                    int up = 0, left = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
