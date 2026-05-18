79ea83b16624fb8aad51cdf2ea1400402f4dc331
Bhavay Batra <btech10549.23@bitmesra.ac.in>
2026-05-18 23:15:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (28.91%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,34 +1,34 @@
 class Solution {
-    // int func(vector<vector<int>>& matrix, int i, int j ,vector<vector<int>>& dp){
-        
-    //     // if((i>0 && j< 0) || (i<0 && j>0))return 0;
-        
-    //     // if(matrix[i][j] == 1) return 0;
-    //     // if(i==0 && j ==0) return 1;
-    //     // if(dp[i][j] != -1) return dp[i][j];
-    //     // return dp[i][j] = func(matrix, i-1, j, dp) + func(matrix, i,j-1, dp);
+    // int helperFunc(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
+    //     if(i ==0 && j== 0&& grid[i][j] == 0) return 1;
+    //     if(i<0 || j<0 ||grid[i][j] == 1) return 0;
+    //     if(dp[i][j]!= -1)return dp[i][j];
+
+    //     int left = helperFunc(i-1,j,grid,dp);
+    //     int up = helperFunc(i, j-1, grid, dp);
+    //     return dp[i][j] = left + up;
     // }
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        // return func(matrix, m-1, n-1, dp);
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j <n ; j++){
-                if(matrix[i][j] == 1){
-                    dp[i][j] = 0;
-                    continue;
-                }
-                else if(i == 0&& j == 0) dp[0][0] = 1;
-                else{
-                    int up = 0, left = 0;
-                    if(i > 0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
-                    dp[i][j] = up+left;
-                }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
 
+        // return helperFunc(m-1,n-1, obstacleGrid, dp);
+
+        if(grid[0][0] == 0) dp[0][0] = 1;
+        else dp[0][0] = 0;
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j < n; j++){
+                if(i ==0 && j == 0) continue;
+                int left =0, up = 0;
+                if(i > 0 && grid[i][j] == 0) left = dp[i-1][j];
+                if(j> 0 && grid[i][j] == 0) up = dp[i][j-1];
+
+                dp[i][j] = left + up;
             }
         }
+
         return dp[m-1][n-1];
     }
 };
