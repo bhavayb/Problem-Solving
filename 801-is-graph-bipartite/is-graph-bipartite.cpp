3e707b505d7b15ad41f8c142464c1f25eabc1214
Bhavay Batra <btech10549.23@bitmesra.ac.in>
2026-06-14 13:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (59.58%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<int> vis(V, -1);
-        // queue<int> q;
-        for(int i = 0 ; i < V; i++){
-            if(vis[i] == -1){
-                vis[i] = 0;
+        int n = graph.size();
+        vector<int> color(n, -1);
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                color[i] = 0;
                 queue<int> q;
                 q.push(i);
-                while(!q.empty()){
+                while (!q.empty()) {
                     int node = q.front();
                     q.pop();
-                    for(int j = 0;  j < graph[node].size(); j++){
-                        if(vis[graph[node][j]] == -1){
-                            if(vis[node] == 1) vis[graph[node][j]] = 0;
-                            else vis[graph[node][j]] = 1;
-
-                            q.push(graph[node][j]);
+                    for (auto it : graph[node]) {
+                        if (color[it] == -1) {
+                            q.push(it);
+                            color[it] = !color[node];
+                        } else if (color[it] == color[node]) {
+                            return false;
                         }
-                        else if(vis[graph[node][j]] == vis[node] ) return false;
                     }
                 }
             }
         }
         return true;
-
     }
 };
