a7c5048dd3b44c5194349e339b15d642c33aa25a
Bhavay Batra <btech10549.23@bitmesra.ac.in>
2026-06-16 12:43:53 +0530

    Time: 2 ms (75.28%) | Memory: 19.1 MB (14.85%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjlist(numCourses);
-        for(int i = 0; i< prerequisites.size(); i++){
-            int u = prerequisites[i][0];
-            int v= prerequisites[i][1];
-            adjlist[v].push_back(u);
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses);
+        for(auto it: prerequisites){
+            int a = it[0];
+            int b = it[1];
+            adj[b].push_back(a);
+            indegree[a]++;
         }
-        vector<int> indegree(numCourses,0 );
-        for(int i = 0; i < numCourses; i++){
-            for(auto it: adjlist[i]){
-                indegree[it]++;
-            }
-        }
+        // topo sort
         queue<int> q;
-        for(int i = 0; i < indegree.size();i++){
+        for(int i = 0; i < numCourses; i++){
             if(indegree[i] == 0) q.push(i);
         }
         vector<int> ans;
         while(!q.empty()){
-            int node = q.front();
+            int node =q.front();
             q.pop();
             ans.push_back(node);
-            for(int i = 0; i < adjlist[node].size(); i++){
-                indegree[adjlist[node][i]]--;
-                if(indegree[adjlist[node][i]] == 0) q.push(adjlist[node][i]);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
             }
         }
-        if(ans.size() == numCourses){
-            return ans;
-        }else return {};
+        //sort(ans.begin(), ans.end());
+        if(ans.size() != numCourses) return {};
+        return ans;
     }
 };
