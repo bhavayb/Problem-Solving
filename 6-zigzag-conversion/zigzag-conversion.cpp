57de3becf13df8e00e747b45c318f31572139fb3
Bhavay Batra <btech10549.23@bitmesra.ac.in>
2026-07-17 03:15:51 +0530

    Time: 3 ms (76.68%) | Memory: 14.2 MB (26.20%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        
-        if (numRows == 1) return s;
+        if(numRows == 1 || s.length() <= numRows) return s;
+        vector<string> ans(numRows, "");
 
-        vector<vector<char>> zigzag(numRows, vector<char>(s.length(), ' '));
-        
-        int i = 1, k = 0; 
-        zigzag[0][0] = s[0];
-
+        int i = 0;
+        int flag = 0;
+        int j = 0;
         while(i < s.length()){
-            int j = 1;
-            while(j < numRows && i < s.length()){
-                zigzag[j][k] = s[i++];
-                j++;
+            if(flag == 0){
+                while( j < numRows && i < s.length()){
+                    ans[j] += s[i];
+                    j++; i++;
+                }
+                flag = 1;
             }
-            j = j - 2;
-
-            while(j >= 0 && i < s.length()){
-                k++;
-                zigzag[j][k] = s[i++];
-                j--;
-            }
-        }
-        string ans = "";
-        for(int r = 0; r < numRows; r++){
-            for(int c = 0; c < zigzag[0].size(); c++){
-                if(zigzag[r][c] != ' ')
-                    ans += zigzag[r][c];
+            j = j-2;
+            if(flag == 1){
+                while(j > 0 && i < s.length()){
+                    ans[j] += s[i];
+                    j--; i++;
+                }
+                flag = 0;
             }
         }
-
-        return ans;
+        string final = "";
+        for(auto it :  ans ){
+            final += it;
+        }
+        return final;
     }
 };
-
